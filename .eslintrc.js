fc2057abc848596763d5870f75a6cd71661f8ae5
Francesco Maida <francescomaida91@gmail.com>
2021-01-23 10:49:58 +0100

    fixed eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
   rules: {
     // enable additional rules
     indent: ['error', 2],
-    'linebreak-style': ['error', 'ansi'],
+    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always'],
+    semi: 'off',
+    'no-extra-semi': 'off',
 
     // override default options for rules from base configurations
-    'comma-dangle': ['error', 'always'],
+    'comma-dangle': ['error', 'never'],
     'no-cond-assign': ['error', 'always'],
 
     // disable rules from base configurations
     'no-console': 'off',
-  },
+    'no-unused-vars': 'warn',
+    'no-undef': 'off',
+
+    '@typescript-eslint/no-extra-semi': ['error'],
+    '@typescript-eslint/semi': ['error']
+  }
 };
